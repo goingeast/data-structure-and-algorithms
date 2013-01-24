3bc8b71f041ed2d40f1241fcce193d2e1b4e4ba6
goingeast <haoru.zhao@gmail.com>
2013-01-24 12:31:54 -0500

    add makefile

---
+++ DataStructures/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: haoruzhao
+ *
+ * Created on January 24, 2013, 12:17 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
