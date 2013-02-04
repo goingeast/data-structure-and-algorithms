fd726a74bbe604ecdb66520c9f015ea92c6d483c
goingeast <haoru.zhao@gmail.com>
2013-02-03 21:18:37 -0500

    Add BSTTree no test

--- DataStructures/BinaryTree/BSTTree.h
+++ DataStructures/BinaryTree/BSTTree.h
@@ -7,8 +7,115 @@
 
 #ifndef BSTTREE_H
 #define	BSTTREE_H
+#include <iostream>
+
+template<typename Comparable>
+class BinarySearchTree{
+    public:
+        BinarySearchTree()
+            :root(NULL){
+            
+        }
+        BinarySearchTree(const BinarySearchTree & rhs)
+            :root(NULL){
+            *this = rhs;
+        }
+        const BinarySearchTree & operator=(const BinarySearchTree & rhs);
+        ~BinarySearchTree(){
+            makeEmpty();
+        };
+        
+        const Comparable& findMin() const{
+            if(isEmpty()){
+                return NULL;
+            }else{
+                return _findMin(root)->element;
+            }
+        };
+        
+        const Comparable& findMax() const{
+            if(isEmpty()){
+                return NULL;
+            }else{
+                return _findMax(root)->element;
+            }
+        };
+        bool contains(const Comparable& x) const;
+        bool isEmpty() const{
+            return root == NULL;
+        };
+        void makeEmpty(){
+            _makeEmpty(root);
+        };
+        void insert(const Comparable& x){
+            _insert(x, root);
+        };
+        
+        void remove(const Comparable& x);
+        void removeMin();
+        
+    private:
+       struct BinarySearchNode{
+           Comparable element;
+           BinarySearchNode* left;
+           BinarySearchNode* right;
+           BinarySearchNode(const Comparable& theElement, BinarySearchNode* L = NULL, BinarySearchNode* R= NULL )
+                :element(theElement), left(L), right(R){
+           }
+       };
+       
+       BinarySearchNode* root;
+       void _insert(const Comparable& x, BinarySearchNode* & t);
+       BinarySearchNode* _findMin(BinarySearchNode* t) const{
+            if(t == NULL){
+                return NULL;
+            }else if(t->left == NULL){
+                return t;
+            }
+            _findMin(t);
+        };
+       BinarySearchNode* _findMax(BinarySearchNode* t) const;
+       void _makeEmpty(BinarySearchNode* & t);
+};
 
 
+template<typename Comparable>
+void BinarySearchTree<Comparable>::_insert(const Comparable& x, BinarySearchNode* & temp){
+    if(temp == NULL){
+        temp = new BinarySearchNode(x);
+    }else if(temp->element <= x){
+        _insert(x, temp->right);
+    }else if(temp->element > x){
+        _insert(x, temp->left);
+    }
+}
+template<typename Comparable>
+void BinarySearchTree<Comparable>::_makeEmpty(BinarySearchNode* & t){
+    if(t != NULL){
+        _makeEmpty(t->left);
+        _makeEmpty(t->right);
+        delete t;
+    }
+    t = NULL;
+}
+//template<typename Comparable>
+//BinarySearchNode* BinarySearchTree<Comparable>::_findMin(BinarySearchNode* t) const{
+//    if(t == NULL){
+//        return NULL;
+//    }else if(t->left == NULL){
+//        return t;
+//    }
+//    _findMin(t);
+//}
 
+template<typename Comparable>
+BinarySearchNode* BinarySearchTree<Comparable>::_findMax(BinarySearchNode* t) const{
+    if(t == NULL){
+        return NULL;
+    }else if(t->right == NULL){
+        return t;
+    }
+    _findMax(t);
+}
 #endif	/* BSTTREE_H */
 
--- DataStructures/main.cpp
+++ DataStructures/main.cpp
@@ -9,6 +9,9 @@
 #include "StackArray/QueueList.h"
 #include "StackArray/StackList.h"
 #include "LinkedList/LinkedList.h"
+//#include "BinaryTree/BSTTree.h"
+#include "BinaryTree/BinaryNode.h"
+#include "BinaryTree/BinaryTree.h"
 #include <iostream>
 using namespace std;
 
@@ -18,9 +21,29 @@ using namespace std;
 int main(int argc, char** argv) {
     //Stack<int> f;
     //f.Push(1);
-    linkedList<int> list;
-    list.addToHead(4);
+    //linkedList<int> list;
+    //list.addToHead(4);
     
+    BinaryTree<int> t1( 1 );
+    BinaryTree<int> t3( 3 );
+    BinaryTree<int> t5( 5 );
+    BinaryTree<int> t7( 7 );
+    BinaryTree<int> t2;
+    BinaryTree<int> t4;
+    BinaryTree<int> t6;
+
+    
+    t2.Merge( 2, t1, t3 );
+    t6.Merge( 6, t5, t7 );
+    t4.Merge( 4, t2, t6 );
+
+    cout << "t4 should be perfect 1-7; t2 empty" << endl;
+    cout << "----------------" << endl;
+    cout << "t4 -- in, post, pre, twice each, level once" << endl;
+    t4.PrintInOrder();
+ //   BinarySearchTree<int> t;
+ //   t.insert(1);
+ //   t.insert(2);
 //    Queue<int> q;
 // 
 // 	for( int j = 0; j < 8; j++ )
@@ -31,6 +54,7 @@ int main(int argc, char** argv) {
 // 		while( !q.isEmpty( ) )
 // 			cout << q.dequeue( ) << endl;
 // 	}
+    
    return 0;
 }
 
