5257fb40336edea51ef28456534d77f5f5baba32
goingeast <haoru.zhao@gmail.com>
2013-02-07 23:27:29 -0500

    add avl tree

--- DataStructures/BinaryTree/AVLTree.h
+++ DataStructures/BinaryTree/AVLTree.h
@@ -59,6 +59,7 @@ class AVLTree{
         
         void insert(const Comparable& x){ // insert x
             _insert(x, root);
+            
         }
         
         void remove(const Comparable& x){ // remove
@@ -74,19 +75,67 @@ class AVLTree{
             AVLTreeNode(const Comparable& theElement, AVLTreeNode* L = NULL, AVLTreeNode* R= NULL, int h = 0)
                  :element(theElement), left(L), right(R), height(h){
             }
+            
         };
-       
+        int height( AVLTreeNode *t ) const
+        {
+            return t == NULL ? -1 : t->height;
+        }
+        
+        int max(int r, int l) const{
+            return r > l? r:l;
+        }
+        
         AVLTreeNode* root; // root
         void _insert(const Comparable& x, AVLTreeNode* & temp){ // insert helper
              if(temp == NULL){
                  temp = new AVLTreeNode(x);
-             }else if(temp->element <= x){ // equal put right subtree
-                 _insert(x, temp->right);
-             }else if(temp->element > x){
+             }else if(temp->element > x){ // equal put right subtree
                  _insert(x, temp->left);
+                 if(height(temp->left) - height(temp->right) == 2){
+                     if(temp->left->element > x){
+                         left_left_rotation(temp);
+                     }else{
+                         left_right_rotation(temp);
+                     }
+                 }
+             }else if(temp->element <= x){
+                 _insert(x, temp->right);
+                 if(height(temp->right) - height(temp->left) == 2){
+                     if(temp->right->element <= x){
+                         right_right_rotation(temp);
+                     }else{
+                         right_left_rotation(temp);
+                     }
+                 }
              }
+             temp->height = max(height(temp->left), height(temp->right)) +1;
         }
-        
+        void left_left_rotation(AVLTreeNode*& k2){
+            AVLTreeNode* k1 = k2->left;
+            k2->left = k1->right;
+            k1->right = k2;
+            k2->height = max(height(k2->left), height(k2->right))+1;
+            k1->height = max(height(k1->left), k2->height)+1;
+            k2 = k1;
+        }
+        void right_right_rotation(AVLTreeNode* & k1){
+            AVLTreeNode* k2 = k1->right;
+            k1->right = k2->left;
+            k2->left = k1;
+            k1->height = max(height(k1->left), height(k1->right))+1;
+            k2->height = max(height(k2->right), k1->height) +1;
+            k1 = k2;
+            
+        }
+        void left_right_rotation(AVLTreeNode* & k3){
+            right_right_rotation(k3->left);
+            left_left_rotation(k3);
+        }
+        void right_left_rotation(AVLTreeNode* & k1){
+            left_left_rotation(k1->right);
+            right_right_rotation(k1);
+        }
         AVLTreeNode* _findMin(AVLTreeNode* t) const{ // findMin helper
             if(t == NULL){
                 return NULL;
