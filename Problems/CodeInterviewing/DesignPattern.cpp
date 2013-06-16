67a68ff339c43aae1a27e6870efc167758a3d900
goingeast <haoru.zhao@gmail.com>
2013-06-15 21:05:53 -0700

    singleton

---
+++ Problems/CodeInterviewing/DesignPattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class singleton{
+public:
+    static singleton& getInstance(){
+        static singleton S;
+        return S;
+    }
+    void doSomething(){
+        count+=2;
+    }
+    void print(){
+        cout << count;
+    }
+private:
+    int count;
+    singleton(){};
+    singleton(const singleton &);
+    const singleton& operator=(const singleton &);
+
+};
+
+int main(){
+
+    singleton::getInstance().doSomething();
+    singleton::getInstance().print();
+    singleton::getInstance().doSomething();
+    singleton::getInstance().print();
+
+}
