105aed7379cd182618e7eb3752c2b8830451a813
goingeast <haoru.zhao@gmail.com>
2013-07-28 20:09:58 -0700

    update

--- Problems/CodeInterviewing/BinaryTree.cpp
+++ Problems/CodeInterviewing/BinaryTree.cpp
@@ -1,6 +1,8 @@
 // haoru zhao
 // Binary Tree problem
 
+
+// Tree defination
 #include<iostream>
 #include<deque>
 #include<queue>
@@ -49,6 +51,7 @@ void levelTraverse(TreeNode* &node){
     while(!iterateQueue.empty()){
         TreeNode* temp = iterateQueue.front();
         cout << temp->data;
+        
         if(temp->left)
             iterateQueue.push(temp->left);
         if(temp->right)
@@ -202,14 +205,19 @@ bool check2Sum(unordered_map<int, TreeNode*>& hashmap, TreeNode* node, int k){
         return false;
     if(hashmap.count(k - node->data) == 0){
         hashmap.insert(make_pair<int, TreeNode*>(node->data, node));
-        check2Sum(hashmap, node->left, k);
-        check2Sum(hashmap, node->right, k);
+        
     }else{
         cout<< node->data;
         cout << hashmap.find(k - node->data)->first;
     }
+    check2Sum(hashmap, node->left, k);
+    check2Sum(hashmap, node->right, k);
 }
 
+bool check2SumBinaryTree(int k, TreeNode* root){
+    unordered_map<int, TreeNode*> hashmap;
+    return check2Sum(hashmap, root,k);
+}
 /*
  * the function returns true if there is a pair with sum equals to target sum
  * 
@@ -226,7 +234,7 @@ void inorderTraverseWithoutRercursive( TreeNode* root){
     while(done == false){
         if(currNode != NULL){
             aStack.push(currNode);
-            cout << currNode->data;
+            //cout << currNode->data;
             currNode = currNode->left;
         }else{
             if(!aStack.empty()){
@@ -241,6 +249,60 @@ void inorderTraverseWithoutRercursive( TreeNode* root){
     }
 }
 
+bool check2Sum( TreeNode* root, int k){
+    
+    stack<TreeNode*> leftStack;
+    stack<TreeNode*> rightStack;
+    
+    TreeNode* leftVal = NULL, *rightVal = NULL;
+    TreeNode* curr1 = root, *curr2 = root;
+    
+    bool done1 = false, done2 = false;
+    while(true){
+        
+        while(done1 == false){
+            if(curr1 != NULL){
+                leftStack.push(curr1);
+                curr1 = curr1->left;
+            }else{
+                if(!leftStack.empty()){
+                    curr1 = leftStack.top();
+                    leftStack.pop();
+                    leftVal = curr1;
+                    curr1 = curr1->right;
+                    done1 = true;
+                }else
+                    done1 = true;
+            }    
+        }
+        
+        while(done2 == false){
+            if(curr2 != NULL){
+                rightStack.push(curr2);
+                curr2 = curr2->right;
+            }else{
+                if(!rightStack.empty()){
+                    curr2 = rightStack.top();
+                    rightStack.pop();
+                    rightVal = curr2;
+                    curr2 = curr2->left;
+                    done2 = true;
+                }else
+                    done2 = true;
+            }
+        }
+        if((leftVal != rightVal) && (leftVal->data + rightVal->data) == k){
+            cout << leftVal->data << rightVal->data << endl;
+            return true;
+        }else if((leftVal->data + rightVal->data) > k){
+            done2 = false;
+        }else if((leftVal->data + rightVal->data) < k){
+            done1 = false;
+        }
+        if(leftVal->data > rightVal->data || leftVal == rightVal)
+            return false;
+    }
+}
 void preorderTraverseIterative(TreeNode* root){
     if(root == NULL)
         return;
@@ -257,21 +319,194 @@ void preorderTraverseIterative(TreeNode* root){
     }
 }
 
-bool check2SumBinaryTree(int k, TreeNode* root){
-    unordered_map<int, TreeNode*> hashmap;
-    return check2Sum(hashmap, root,k);
+void zigZagTraverse(TreeNode* root){
+    
+    stack<TreeNode*> stack1;
+    stack<TreeNode*> stack2;
+    
+    stack1.push(root);
+    
+    while(!stack1.empty() || !stack2.empty()){
+        while(!stack1.empty()){
+            TreeNode* node = stack1.top();
+            stack1.pop();
+            cout << node->data;
+            if(node->left)
+                stack2.push(node->left);
+            if(node->right)
+                stack2.push(node->right);
+        }
+    
+        while(!stack2.empty()){
+            TreeNode* node = stack2.top();
+            stack2.pop();
+            cout << node->data;
+            if(node->right)
+                stack1.push(node->right);
+            if(node->left)
+                stack1.push(node->left);
+            
+        }
+    }
 }
 
-//#ifdef BINARY_TREE
+
+/*------------------------------------------------
+ */
+bool matchTree(TreeNode* t1, TreeNode* t2){
+    if(t1 == NULL && t1 == NULL)
+        return true;
+    if(t1 == NULL || t1 == NULL)
+        return false;
+    
+    if(t1->data != t1->data)
+        return false;
+    else
+        return matchTree(t1->left, t2->left)&& matchTree(t1->right, t2->right);
+}
+
+bool isSubTree(TreeNode* t1, TreeNode* t2){
+    if(t1 == NULL)
+        return false;
+    if(t1->data == t2->data)
+        if(matchTree(t1, t2))
+            return true;
+    
+    return isSubTree(t1->left, t2) || isSubTree(t2->right, t2);
+}
+
+bool containsTree(TreeNode* t1, TreeNode* t2){
+    if(t2 == NULL)
+        return true;
+    return isSubTree(t1, t2);
+}
+/*------------------------------------------------
+ */
+void postorderTraverseIterative(TreeNode* root){
+    if(root == NULL)
+        return;
+    stack<TreeNode*> aStack;
+    do{
+        while(root != NULL){
+            if(root->right != NULL)
+                aStack.push(root->right);
+            aStack.push(root);
+            root= root->left;
+        }
+        
+        root = aStack.top(); 
+        aStack.pop();
+        
+        if(root->right != NULL && aStack.top() == root->right){
+            aStack.pop();
+            aStack.push(root);
+            root = root->right;
+        }else{
+            cout << root->data;
+            root = NULL;
+        }
+        
+            
+    }while(!aStack.empty());
+}
+
+/*least common ancestor for BST
+ * we can use property of BST to check if current node's value
+ * is between two node.
+ */
+
+TreeNode* findLeastCommonAncestor(TreeNode* root, int n1, int n2){
+    /* If we have reached a leaf node then LCA doesn't exist 
+     If root->data is equal to any of the inputs then input is 
+     not valid. For example 20, 22 in the given figure */ 
+    if(root == NULL || root->data == n1 || root->data == n2)
+        return NULL;
+    /* If any of the input nodes is child of the current node
+     we have reached the LCA. For example, in the above figure
+     if we want to calculate LCA of 12 and 14, recursion should 
+     terminate when we reach 8*/
+    if((root->right != NULL) && root->right->data == n1 || root->right->data == n2)
+        return root;
+    if((root->left != NULL) && root->left->data == n1 || root->left->data == n2)
+        return root;
+    
+    if(root->data > n1 && root->data < n2)
+        return root;
+    if(root->data > n1 && root->data > n2)
+        return findLeastCommonAncestor(root->left, n1, n2);
+    if(root->data < n1 && root->data < n2)
+        return findLeastCommonAncestor(root->right, n1, n2);
+
+}
+/* least common ancestor for Binary Tree
+ */
+TreeNode* findLeastCommanAncestorBT(TreeNode* root, int n1, int n2){
+    
+}
+
+
+
+TreeNode* buildTree(int pre[], char preLN[], int size){
+    static int _index = 0;
+    if(_index == size)
+        return NULL;
+    TreeNode* node = new TreeNode(pre[_index]);
+    if(preLN[_index++] == 'N'){
+        node->left  = buildTree(pre, preLN, size);
+        node->right = buildTree(pre, preLN, size);
+    }
+    return node;
+}
+
+
+
+
+void FormDLLWithLeafNodes(TreeNode *root)
+{
+    if(root == NULL)
+        return;
+    /*
+      Again we are making use of In-Order traversal to achieve this.
+      Since we are asked to connect leaf nodes from left to right,
+      we can traverse through the tree using In-Order and check for 
+      leaf nodes and connect them as DLL. We can keep track of previous
+      node using a static prevNode variable.
+    */
+    static TreeNode* prevNode = NULL;
+    FormDLLWithLeafNodes(root->left);
+    // Check if it is a leaf node
+    if( ( root->left == NULL ) &&
+        ( root->right == NULL ) )
+    {
+        if(prevNode == NULL)
+        {
+            prevNode = root;
+            cout<<root->data;
+        }
+        else
+        {
+            // Connect the leaf nodes here
+            prevNode->right = root;
+            root->left     = prevNode;
+            prevNode         = root;
+            
+            cout<<"<=>"<<root->data;
+        }
+    }
+    FormDLLWithLeafNodes(root->right);
+}
+
+#ifdef BINARY_TREE
 
 int main(){
     TreeNode* a1 = new TreeNode(0);
-    TreeNode* a = new TreeNode(1,a1, NULL);
+    TreeNode* a2 = new TreeNode(7);
+    TreeNode* a = new TreeNode(1,a1, a2);
     
     TreeNode* b = new TreeNode(4);
     TreeNode* c = new TreeNode(2,a,NULL);
     
-    TreeNode* d = new TreeNode(5);
+    TreeNode* d = new TreeNode(5,b,NULL);
     TreeNode* root = new TreeNode(3,c,d);
     
 //    cout << isBST(root);
@@ -291,9 +526,19 @@ int main(){
 //        cout << "balanced";
 //    else
 //        cout << "no";
-    //check2SumBinaryTree(7, root);
-    inorderTraverseWithoutRercursive(root);
-    preorderTraverseIterative(root);
+    //check2SumBinaryTree(6, root);
+    cout << "--------";
+    //check2Sum(root,6);
+    //inorderTraverseWithoutRercursive(root);
+    //preorderTraverseIterative(root);
+    //cout << findLeastCommonAncestor(root, 5, 4)->data;
+    
+//    int pre[]= {10, 30, 20, 5,15};
+//    char preLN[] = {'N','N','L','L','L'};
+//    TreeNode* tmp = buildTree(pre, preLN, 5);
+//    cout <<"";
+    
+    FormDLLWithLeafNodes(root);
 }
 
-//#endif
+#endif
--- Problems/CodeInterviewing/BitProblem.cpp
+++ Problems/CodeInterviewing/BitProblem.cpp
@@ -64,7 +64,52 @@ int getSetBitsFromOneToN1(int N){
     return ans;
 }
 
-#ifdef BIT_VECTOR
+
+/*
+ * 32bit integer reverse
+ * 0x1010 1010 1010 1010 1010 1010 1010 1010
+ * 0x0101 0101 0101 0101 0101 0101 0101 0101
+ * 0x1100 1100 1100 1100 1100 1100 1100 1100
+ * 0x0011 0011 0011 0011 0011 0011 0011 0011
+ * 
+ * 
+ * 0x1111 0000 1111 0000 1111 0000 1111 0000
+ * 0x0000 1111 0000 1111 0000 1111 0000 1111
+ * 0x1111 1111 0000 0000 1111 1111 0000 0000
+ * 0x0000 0000 1111 1111 0000 0000 1111 1111
+ */
+unsigned int reverse(unsigned int x)
+{
+    x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+    x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
+    x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
+    x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
+    return((x >> 16) | (x << 16));
+
+}
+
+int swapBits(unsigned int x, unsigned int p1, unsigned int p2, unsigned int n)
+{
+    /* Move all bits of first set to rightmost side */
+    unsigned int set1 =  (x >> p1) & ((1U << n) - 1);
+ 
+    /* Move all bits of second set to rightmost side */
+    unsigned int set2 =  (x >> p2) & ((1U << n) - 1);
+ 
+    /* XOR the two sets */
+    unsigned int xor1 = (set1 ^ set2);
+ 
+    /* Put the xor bits back to their original positions */
+    xor1 = (xor1 << p1) | (xor1 << p2);
+ 
+    /* XOR the 'xor' with the original number so that the 
+       two sets are swapped */
+    unsigned int result = x ^ xor1;
+ 
+    return result;
+}
+
+//ifdef BIT_VECTOR
 int main(){
 //    bitSet a(3200);
 //    
@@ -77,11 +122,13 @@ int main(){
 //            
 //        }
 //    }
-    cout << getSetBitsFromOneToN(6);
-    if(1>0){
-        cout<<'k';
-    }
+//    cout << getSetBitsFromOneToN(6);
+//    if(1>0){
+//        cout<<'k';
+//    }
+    size_t i = 6;
+    cout << std::hex<<swapBits(6, 1, 3, 1);
 }
-#endif
+//#endif
   
 
