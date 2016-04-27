373ae4b196fb51e94df00d334dbfe8dd7d98ccff
haoru zhao <haoru.zhao@gmail.com>
2016-04-27 00:14:39 -0700

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution.cpp
+++ 139-Word-Break/solution.cpp
@@ -3,12 +3,12 @@ public:
     bool wordBreak(string s, unordered_set<string>& wordDict) {
         bool* res = new bool[s.size()+1]();
         res[0] = true;
-        for(int i=1; i<= s.size(); i++){
+        for(int i=0; i< s.size(); i++){
             
-            for(int j = i -1; j>=0; j--){
+            for(int j = 0; j<=i; j++){
                 
-                if(res[j] && wordDict.find(s.substr(j, i-j))!=wordDict.end()){
-                    res[i] = true;
+                if(res[j] && wordDict.find(s.substr(j, i-j+1))!=wordDict.end()){
+                    res[i+1] = true;
                 }
             }
         }
