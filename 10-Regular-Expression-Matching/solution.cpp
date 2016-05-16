34fa5eceeaa91cd2892cce5374c53cb440e55d0a
haoru zhao <haoru.zhao@gmail.com>
2016-05-16 16:25:19 -0700

    [10. Regular Expression Matching][Compile Error]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.cpp
+++ 10-Regular-Expression-Matching/solution.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.size();
-        int n = p.size();
-        
-        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
-        
-        dp[0][0] = true;
-       
-        // p[0.., j - 3, j - 2, j - 1] matches empty iff p[j - 1] is '*' and p[0..j - 3] matches empty
-        for (int j = 1; j <= n; j++)
-            dp[0][j] = (j > 1 && '*' == p[j - 1] && dp[0][j - 2]);
-        
-        for(int i = 1; i <= m; ++i){
-            for(int j = 1; j <= n; ++j){
-                if(p[j-1] != '*'){
-                    dp[i][j] = dp[i-1][j-1] && (s[i-1] == p[j-1] || '.' == p[j-1]);
-                }else{
-                    dp[i][j] = dp[i][j-2] || ((s[i-1] == p[j-2] || '.' == p[j-2])&& dp[i-1][j]);
-                }
-            }
-        }
-        return dp[m][n];
+         bool isMatch(string s, string p) {
+        if (p.empty())    return s.empty();
+
+        if ('*' == p[1])
+            // x* matches empty string or at least one character: x* -> xx*
+            // *s is to ensure s is non-empty
+            return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
+        else
+            return !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1));
+    }
     }
 };
