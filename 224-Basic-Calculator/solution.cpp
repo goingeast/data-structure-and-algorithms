96d355e9a309b4faf3fb68a6a23442ed1ed7a0c5
haoru zhao <haoru.zhao@gmail.com>
2016-06-16 14:42:50 -0700

    [224. Basic Calculator][Accepted]committed by LeetCode Extension

--- 224-Basic-Calculator/solution.cpp
+++ 224-Basic-Calculator/solution.cpp
@@ -14,15 +14,11 @@ public:
       for(int i = 0; i < s.size(); ++i){
         if(isdigit(s[i])){
           number = number*10 + (s[i] - '0');
-        }
-        
-        if(s[i] == '-'|| s[i] == '+'){
+        }else if(s[i] == '-'|| s[i] == '+'){
           total += lastSign * number;
           number = 0;
           lastSign = (s[i] == '-'? -1:1) * calculator.top(); // like we remove parenthese, we need previous sign
-        }  
-        
-        if(s[i] == '('){
+        }else if(s[i] == '('){
           calculator.push(lastSign);
         }else if(s[i] == ')'){
           calculator.pop();
