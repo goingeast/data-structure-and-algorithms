797b50d7f0b9dcce67ba5c9ce52b1a2705db84cd
goingeast <haoru.zhao@gmail.com>
2018-12-06 20:57:57 -0800

    add basic calculatorII

---
+++ Problems/CodeInterviewing/leetcodeLock/basic_calculator.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int calculate(string s) {
+        /* just do operation like remove all parenthese*/
+      /* use stack to track parenthese*/
+
+      stack<char> calculator;
+      calculator.push(1);
+
+      int total = 0;
+      int number = 0;
+      int lastSign = 1;
+
+      for(int i = 0; i < s.size(); ++i){
+        if(isdigit(s[i])){
+          number = number*10 + (s[i] - '0');
+        }
+
+        if(s[i] == '-'|| s[i] == '+'){
+          total += lastSign * number;
+          number = 0;
+          lastSign = (s[i] == '-'? -1:1) * calculator.top(); // like we remove parenthese, we need previous sign
+        }
+
+        if(s[i] == '('){
+          calculator.push(lastSign);
+        }else if(s[i] == ')'){
+          calculator.pop();
+        }
+      }
+      total += lastSign* number;
+      return total;
+    }
+};
---
+++ Problems/CodeInterviewing/leetcodeLock/basic_calculatorII.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> calculator;
+        int number = 0;
+        int res = 0;
+        char sign = '+';
+        for(int i =0; i < s.size(); ++i){
+          if(isdigit(s[i])){
+            number += number*10 + s[i] - '0';
+          }
+          if(!isdigit(s[i]) || i = s.size() -1){
+            if(sign == '-'){
+              calculator.push(-number);
+            }else if(sign == '+'){
+              calculator.push(number);
+            }else if(sign == '*'){
+              res = calculator.top()*number;
+              calculator.pop();
+              calculator.push(res);
+
+            }else if(sign == '/'){
+              res = calculator.top()/number;
+              calculator.pop();
+              calculator.push(res);
+            }
+            number = 0;
+            sign = s[i];
+          }
+        }
+
+        while(!calculator.empty()){
+          res+=calculator.top();
+          calculator.pop();
+        }
+        return res;
+    }
+};
